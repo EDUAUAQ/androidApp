e807d70505c5de5b4bc927c3814978e2b0952fba
DiegoLuna953 <dluna10@alumnos.uaq.mx>
2024-09-10 09:39:31 -0600

    Initial commit

---
+++ app/src/main/java/com/topico1/bancoedua/register.java
@@ -0,0 +1,32 @@
+package com.topico1.bancoedua;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class register extends AppCompatActivity {
+
+    public void irLogin(View view){
+        Log.i("Info","Boton presionado");
+        Intent miIntent = new Intent(this, login.class);
+        startActivity(miIntent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
