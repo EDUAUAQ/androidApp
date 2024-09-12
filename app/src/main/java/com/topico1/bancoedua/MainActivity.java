f67fa5a7b448c3d4f60883b73e94af4d690f25d4
DiegoLuna953 <dluna10@alumnos.uaq.mx>
2024-09-12 08:56:51 -0600

    Login and Register

--- app/src/main/java/com/topico1/bancoedua/MainActivity.java
+++ app/src/main/java/com/topico1/bancoedua/MainActivity.java
@@ -8,29 +8,14 @@ import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.NavigationUI;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private BottomNavigationView bottomNavView;
-    private NavController navController;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        bottomNavView = findViewById(R.id.nav_view);
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.fragment_container);
-        navController = navHostFragment.getNavController();
-
-        NavigationUI.setupWithNavController(bottomNavView, navController);
 
         ImageView mainLogo = findViewById(R.id.mainLogo);
         TextView welcomeText = findViewById(R.id.welcomeText);
--- app/src/main/java/com/topico1/bancoedua/index.java
+++ app/src/main/java/com/topico1/bancoedua/index.java
@@ -1,21 +1,15 @@
 package com.topico1.bancoedua;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
-
-import com.topico1.bancoedua.databinding.ActivityIndexBinding;
 
 public class index extends AppCompatActivity {
 
@@ -24,11 +18,5 @@ public class index extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_index);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
-
 }
