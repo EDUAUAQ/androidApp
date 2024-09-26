528e324c49ab4ce20fda19537d09b044456fbffb
DiegoLuna953 <dluna10@alumnos.uaq.mx>
2024-09-26 13:00:26 -0600

    Bootm Navigation

--- app/src/main/java/com/topico1/bancoedua/index.java
+++ app/src/main/java/com/topico1/bancoedua/index.java
@@ -1,15 +1,14 @@
 package com.topico1.bancoedua;
 
 import android.os.Bundle;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import androidx.appcompat.widget.Toolbar;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.topico1.bancoedua.databinding.ActivityIndexBinding;
 
 public class index extends AppCompatActivity {
@@ -23,15 +22,17 @@ public class index extends AppCompatActivity {
         binding = ActivityIndexBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        // Configurar el Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_accounts, R.id.navigation_transactions, R.id.navigation_profile)
+                R.id.navigation_home, R.id.navigation_create, R.id.navigation_transactions, R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_index);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
-
 }
